6ddedcfa812a1123b70157af1a4afd299e98afe4
GowthamGthm <gowtham.gthm@gmail.com>
2024-10-31 21:36:35 +0530

    changes

---
+++ src/test/java/com/gthm/api/util/FileUtilityTest.java
@@ -0,0 +1,36 @@
+package com.gthm.api.util;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import java.io.FileNotFoundException;
+
+
+class FileUtilityTest {
+
+    @Test
+    void testLoadFile() throws Exception {
+
+        Resource resource = new ClassPathResource("json/pokemon.json");
+
+        String stringContent = FileUtility.loadFile(resource);
+
+        Assertions.assertThat(stringContent).isNotNull().isNotEmpty();
+        Assertions.assertThat(stringContent).startsWith("{");
+        Assertions.assertThat(stringContent).endsWith("}");
+
+    }
+
+
+    @Test
+    public void invalid_file_resource_type() {
+
+        FileNotFoundException fileNotFoundException = org.junit.jupiter.api.Assertions.assertThrows(FileNotFoundException.class, () -> FileUtility.loadFile(new ByteArrayResource("content".getBytes())));
+        fileNotFoundException.printStackTrace();
+
+    }
+
+}
